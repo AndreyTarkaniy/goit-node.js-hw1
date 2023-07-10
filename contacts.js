9045f51fe95788737966091956b21442cc481b4c
AndreyTarkaniy <endrubass@gmail.com>
2023-07-10 17:53:22 +0300

    exporting getContactsById to index.js

--- contacts.js
+++ contacts.js
@@ -5,11 +5,17 @@ const contactsPath = path.resolve("db", "contacts.json");
 console.log(contactsPath);
 
 export const listContacts = async () => {
-  const data = await fs.readFile(contactsPath, "utf-8");
+  const data = await fs.readFile(contactsPath);
   return JSON.parse(data);
+};
+
+export const getContactById = async contactId => {
+  const allMovies = await listContacts();
+  const oneMovie = allMovies.find(({ id }) => id === contactId);
+  return oneMovie || null;
 };
 // listContacts();
 
 // const contactsService = { listContacts };
 // module.exports = contactsService;
-export default { listContacts };
+export default { listContacts, getContactById };
--- index.js
+++ index.js
@@ -1,16 +1,23 @@
 // console.log("hi Fras,dear friend,mother fucker");
+import argv from "yargs";
 // const argv = require("yargs").argv;
 
 import contactsService from "./contacts.js";
 // const contactsService = require("constants.js");
 
-const invokeActions = async ({ action }) => {
+const invokeActions = async ({ action, id }) => {
   switch (action) {
     case "list":
       const allContacts = await contactsService.listContacts();
       return console.log(allContacts);
+    case "get":
+      const contact = await contactsService.getContactById(id);
+      return console.log(contact);
     default:
       console.log("neponyatka");
   }
 };
-invokeActions({ action: "list" });
+// invokeActions({ action: "list" });
+// invokeActions({ action: "get", id: "AeHIrLTr6JkxGE6SN-0Rw" });
+
+invokeActions(argv);
