fb62e2abc9d33e157b35e635577bc035cb73e721
AndreyTarkaniy <endrubass@gmail.com>
2023-07-10 19:03:23 +0300

    exporting addContact to index.js

--- contacts.js
+++ contacts.js
@@ -1,4 +1,5 @@
 import fs from "fs/promises";
+import { nanoid } from "nanoid";
 import path from "path";
 
 const contactsPath = path.resolve("db", "contacts.json");
@@ -10,12 +11,27 @@ export const listContacts = async () => {
 };
 
 export const getContactById = async contactId => {
-  const allMovies = await listContacts();
-  const oneMovie = allMovies.find(({ id }) => id === contactId);
+  const allContacts = await listContacts();
+  const oneMovie = allContacts.find(({ id }) => id === contactId);
   return oneMovie || null;
 };
+
+export const addContact = async (name, email, phone) => {
+  const allContacts = await listContacts();
+  const newContact = {
+    id: nanoid(),
+    name,
+    email,
+    phone,
+  };
+  allContacts.push(newContact);
+  await fs.writeFile(contactsPath, JSON.stringify(allContacts, null, 2));
+  return newContact;
+  // return allContacts; - поверне масив з доданим контатом
+};
+
 // listContacts();
 
 // const contactsService = { listContacts };
 // module.exports = contactsService;
-export default { listContacts, getContactById };
+export default { listContacts, getContactById, addContact };
--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 // console.log("hi Fras,dear friend,mother fucker");
-import argv from "yargs";
+// import argv from "yargs";
 // const argv = require("yargs").argv;
 
 import contactsService from "./contacts.js";
 // const contactsService = require("constants.js");
 
-const invokeActions = async ({ action, id }) => {
+const invokeActions = async ({ action, id, name, email, phone }) => {
   switch (action) {
     case "list":
       const allContacts = await contactsService.listContacts();
@@ -13,6 +13,9 @@ const invokeActions = async ({ action, id }) => {
     case "get":
       const contact = await contactsService.getContactById(id);
       return console.log(contact);
+    case "add":
+      const newContact = await contactsService.addContact(name, email, phone);
+      return console.log(newContact);
     default:
       console.log("neponyatka");
   }
@@ -20,4 +23,4 @@ const invokeActions = async ({ action, id }) => {
 // invokeActions({ action: "list" });
 // invokeActions({ action: "get", id: "AeHIrLTr6JkxGE6SN-0Rw" });
 
-invokeActions(argv);
+// invokeActions(argv);
